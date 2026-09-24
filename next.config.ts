afa02c67dc3207772aecd586fc981e313c9b2d6e
Ewizell <borisenkov.pave1@yandex.ru>
2026-09-24 10:19:25 +0300

    S3 V1.8

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '10mb',
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.regru.cloud',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
